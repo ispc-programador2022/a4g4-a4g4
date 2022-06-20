cb0f9400e3dc6dd8cc45e95f606b1416ae32b6ed
Juan Diego Coianiz <95014620+diegoCoianiz@users.noreply.github.com>
2022-06-20 20:45:31 -0300

    Update media.py
    elimine la funcion print que ya no es necesaria, y pase como parametro la lista para la funcion media()

--- media.py
+++ media.py
@@ -13,10 +13,8 @@ genrd()
 #EJERCICIO N° 16
 """     calcular la media aritmetica del vector de list    """
 
-def media():
+def media(list):
     counter = 0
     for i in list:
         counter += i
     return counter / len(list)
-
-print(fun16())
