7af2e5a0ad0e53d6c4b53e1c185e0e29658aa9e0
AngelAncillotti1 <107323698+AngelAncillotti1@users.noreply.github.com>
2022-06-22 00:57:07 -0300

    Create n_aleatorios.py
    Generar una lista de 50 numero aleatorios.

---
+++ n_aleatorios.py
@@ -0,0 +1,13 @@
+from ramdom import ramdom
+  import random
+  
+  def lista aleatoria():
+    lista=[]
+      for i in range (0,50):
+        i=random.randit(0,50)
+        lista.append(i)
+   print(lista)
+  
+lista_aleatoria()
+
+
