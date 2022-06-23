3aea0385546a3fae8a825ffea781dbe5fa10522b
Ancillotti Angel <ancillottiangel@gmail.com>
2022-06-22 22:39:02 -0300

    commit.2

--- n_aleatorios.py
+++ n_aleatorios.py
@@ -1,14 +1,14 @@
-from random import random
-  import random
-  
-  def lista aleatoria():
-    lista= []
-      for i in range (0,50):
-        i = random.randint(0,50)
-        lista.append(i)
-   print(lista)
-  
-  
-lista_aleatoria()
+#Ejercicio N°12
+#Función genrnd que retorna una lista con 50 números aleatorios.
+#Alumno: Ancillotti Angel
+#Grupo: A4G4
 
 
+import random
+lista = []
+fin = 50
+for i in range (1, fin+1):
+        lista.append(random.randint(1,50))
+        aleatorios = list(dict.fromkeys(lista))
+
+print("lista",aleatorios)        
