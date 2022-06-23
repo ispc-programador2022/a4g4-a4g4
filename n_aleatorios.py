9793b0bede215e86755b6713cd811a5e1da1c42c
Ancillotti Angel <ancillottiangel@gmail.com>
2022-06-23 00:47:04 -0300

    Commit.3

--- n_aleatorios.py
+++ n_aleatorios.py
@@ -5,10 +5,16 @@
 
 
 import random
-lista = []
-fin = 50
-for i in range (1, fin+1):
+def genrd():
+    lista = []
+    fin = 50
+    for i in range (1, fin+1):
         lista.append(random.randint(1,50))
         aleatorios = list(dict.fromkeys(lista))
 
-print("lista",aleatorios)        
+    print("lista",aleatorios)    
+
+genrd()
+
+#Fin del Ejercicio. 
+
