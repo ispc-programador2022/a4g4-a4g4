283bce51be8741b369615ac7eec507243d98b893
Federico <fedecdoppler@gmail.com>
2022-07-02 16:00:29 -0300

    Agregada función producto y agregadas opciones a bienvenida

--- bienvenida.py
+++ bienvenida.py
@@ -1,10 +1,13 @@
+import time
 from suma import suma
 from potencia import potencia
 from ing2i import ing2i
 from media import media
 from genrd import genrd
 from p11 import p11 as p3
+from producto import producto
 
+lista_cargada = False
 
 def bienvenida():
     ultimaOpcionValida = 31
@@ -20,13 +23,29 @@ def bienvenida():
             print(suma(var1, var2))
         elif opcion == 2:
             print('resta(var1, var2)')
+        elif opcion == 3:
+            print(producto(var1, var2))
+        elif opcion == 4:
+            print('cociente(var1, var2)')
+        elif opcion == 5:
+            print('modulo(var1, var2)')
         elif opcion == 6:
             print(potencia(var1,var2))
+        elif opcion == 7:
+            print('radicacion(var1, var2)')
+        elif opcion == 9:
+            print('p1(var1, var2)')
+        elif opcion == 10:
+            print('p2(var1, var2)')
         elif opcion == 11:
             print(p3)
-        elif opcion == 16:
+        elif opcion == 12:
             list = genrd()
+            lista_cargada = True
+        elif opcion == 16:
             print(media(list))
+
+            start = time.time()
         else:
             print('no esta dentro de las opciones')
             
@@ -34,11 +53,19 @@ def bienvenida():
 def printMenu():
     print("")
     print('Esta aplicacion pertenece al aula 4 grupo 4,vamos a desarrollar diferentes funciones con lo aprendido')
-    print("1. Para Sumar")
-    print("2. para restar")
-    print("6. Para hacer la potencia del 1er valor elevado al 2do valor")
+    print("1.  Función paara sumar")
+    print("2.  Función para restar")
+    print("3.  Función para multiplicar")
+    print("4.  Función para dividir")
+    print("5.  Función para calcular el módulo")
+    print("6.  Función Para hacer la potencia del 1er valor elevado al 2do valor")
+    print("7.  Función para calcular la raíz enésima(segundo valor) del primer valor")
+    print("9.  Para ejecutar la operación de producto y suma con tercer parametro")
+    print("10. Para ejecutar la operación de suma y producto con tercer parametro")
     print("11. Para ejecutar la operación de resta y multiplicación con tercer parametro")
-    print("16. para calcular la media")
+    print("12. Generar lista con 50 números aleatorios")
+    print("13. ")
+    print("12. ")
     print("0. Salir")
     
 
